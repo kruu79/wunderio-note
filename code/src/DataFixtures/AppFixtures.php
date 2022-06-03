01d1f011a73895e678a1c796f3a83bcf3c76f07e
Janis Krumins <krumins1979@gmail.com>
2022-06-03 15:16:55 +0300

    Add test Note data in AppFixtures.php

--- code/src/DataFixtures/AppFixtures.php
+++ code/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,64 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Note;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @param ObjectManager $manager
+     * @throws \Exception
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $notes = [
+            [
+                'id' => 1,
+                'title' => 'First Note',
+                'text' => 'The text of the first note.',
+                'age_in_days' => 3
+            ],
+
+            [
+                'id' => 2,
+                'title' => 'Second Note',
+                'text' => 'The text of the first note.',
+                'age_in_days' => 2
+            ],
+
+            [
+                'title' => 'Third Note',
+                'text' => 'The text of the first note.',
+                'age_in_days' => 1,
+                'id' => 3,
+            ],
+        ];
+
+        foreach ($notes as $noteData) {
+            $this->persistNote($noteData, $manager);
+        }
 
         $manager->flush();
+    }
+
+    /**
+     * Persists Note entity in $manager
+     *
+     * @param $noteData
+     * @param ObjectManager $manager
+     * @throws \Exception
+     */
+    private function persistNote($noteData, ObjectManager $manager)
+    {
+        $note = new Note();
+        $note->setTitle($noteData['title']);
+        $note->setText($noteData['text']);
+
+        $date = (new \DateTimeImmutable())->modify(sprintf('-%d day', $noteData['age_in_days']));
+        $note->setCreatedAt($date);
+
+        $manager->persist($note);
     }
 }
